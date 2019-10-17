ff8189c51a1e028a473dcb4f50ff0dbbac5270d7
DPigeon <D_Pig@encs.concordia.ca>
2019-10-17 13:17:22 -0400

    Ordering Layers & Mana Bar

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
     public void MenuInteractions(string name) {
-        Application.LoadLevel(name);
+        SceneManager.LoadScene(name);
     }
 }
