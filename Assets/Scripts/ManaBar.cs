6be4167df160ce316f644d666ac5b51a16d234bc
DPigeon <D_Pig@encs.concordia.ca>
2019-10-17 15:02:31 -0400

    #9: Adding Mana Filled Sprites & Script

---
+++ Assets/Scripts/ManaBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManaBar : MonoBehaviour
+{
+    [SerializeField]
+    int maxHealth;
+    int index;
+
+    SpriteRenderer[] barFilled; // Array of portions of the sword bar
+
+    void Start() {
+        barFilled = transform.Find("BarFilled").GetComponentsInChildren<SpriteRenderer>();
+        maxHealth = barFilled.Length;
+        index = barFilled.Length - 1;
+    }
+
+    void Update() {
+        
+    }
+
+}
