fa3e00ce3cc6ff67e362c8160bb9e1e0a0d4e2bb
DanielPri <privorotskyd@gmail.com>
2019-10-30 18:06:08 -0400

    Doors and levers now have scripts

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] bool doorClosed = true;
+    Collider2D doorCollider;
+    void Start() {
+        doorCollider = GetComponent<Collider2D>();
+        colliderChange();
+    }
+    public void toggle() {
+
+        doorClosed = !doorClosed;
+
+        //animateDoor
+        colliderChange();
+
+
+    }
+
+    void colliderChange() {
+        if (doorClosed)
+        {
+            doorCollider.enabled = true;
+        }
+        else
+        {
+            doorCollider.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/LeverDoorController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverDoorController : MonoBehaviour
+{
+
+    [SerializeField]
+    List<GameObject> Doors;
+
+   public void toggle(Collider2D col) { 
+            foreach (GameObject Door in Doors) {
+                //TODO add door toggle script
+                Door.GetComponent<Door>().toggle();
+                Debug.Log("YOU'RE IN MY LAYER, uhhh..... lever.");
+            }
+    }
+}
