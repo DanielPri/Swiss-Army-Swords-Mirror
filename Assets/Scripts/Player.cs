93acafb0e87272ab6c12dff92cfe6df2ef0670dd
Maude <maude.braunstein@gmail.com>
2019-10-31 01:34:41 -0400

    #46: Created brick and ice drops as well as pickup animation.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     [SerializeField] float playerSpeed;
     [SerializeField] float jumpForce;
 
+    bool pickingUpSword;
     bool moving;
     bool grounded;
     bool falling;
@@ -31,6 +32,7 @@ public class Player : MonoBehaviour
         playerAnimator.SetBool("isMoving", moving);
         playerAnimator.SetBool("isGrounded", grounded);
         playerAnimator.SetBool("isFalling", falling);
+        playerAnimator.SetBool("isPickingUpSword", pickingUpSword);
     }
 
     void OnTriggerEnter2D(Collider2D col)
@@ -39,7 +41,13 @@ public class Player : MonoBehaviour
         {
             grounded = true;
         }
-        
+        if (col.gameObject.name.Contains("SwordDrop") && !pickingUpSword)
+        {
+            pickingUpSword = true;
+            StartCoroutine(WaitAndPickup(col.gameObject));
+            Debug.Log("Back from sword pickup");
+        }
+
     }
     void OnTriggerStay2D(Collider2D col)
     {
@@ -60,27 +68,51 @@ public class Player : MonoBehaviour
         }
     }
 
+    private IEnumerator WaitAndPickup(GameObject swordGO)
+    {
+        yield return StartCoroutine(HandleSwordPickup(swordGO));
+    }
+
+    private IEnumerator HandleSwordPickup(GameObject swordGO)
+    {
+        Sword swordcomponent = GetComponentInChildren<Sword>();
+        swordcomponent.enabled = false;
+        yield return new WaitForSeconds(2.0f);        
+        SwordInventory si = GetComponentInParent<SwordInventory>();
+        Debug.Log(si);
+        if (si == null)
+        {
+            Debug.Log("Found?");
+        }
+        pickingUpSword = false;
+        swordcomponent.enabled = true;
+    }
+
     private void MovePlayer()
     {
         moving = false;
-        if (Input.GetButton("Left"))
+        if (!pickingUpSword)
         {
-            transform.Translate(-Vector2.right * playerSpeed * Time.deltaTime);
-            transform.localScale = new Vector2(-1, 1);
-            facingDirection = -transform.right;
-            moving = true;
-        }
-        if (Input.GetButton("Right"))
-        {
-            transform.Translate(Vector2.right * playerSpeed * Time.deltaTime);
-            transform.localScale = new Vector2(1, 1);
-            facingDirection = transform.right;
-            moving = true;
-        }
+            if (Input.GetButton("Left"))
+            {
+                transform.Translate(-Vector2.right * playerSpeed * Time.deltaTime);
+                transform.localScale = new Vector2(-1, 1);
+                facingDirection = -transform.right;
+                moving = true;
+            }
+            if (Input.GetButton("Right"))
+            {
+                transform.Translate(Vector2.right * playerSpeed * Time.deltaTime);
+                transform.localScale = new Vector2(1, 1);
+                facingDirection = transform.right;
+                moving = true;
+            }
 
-        if (grounded && Input.GetButtonDown("Jump"))
-        {
-            player.AddForce(Vector2.up * jumpForce);
+            if (grounded && Input.GetButtonDown("Jump"))
+            {
+                player.AddForce(Vector2.up * jumpForce);
+            }
+
         }
     }
 
