9605d6b84c590885325a8debe93b8dd1d90e2d83
DPigeon <D_Pig@encs.concordia.ca>
2019-11-12 03:16:58 -0500

    Adding a Light to the sword

--- Assets/Scripts/Swords/LightSword.cs
+++ Assets/Scripts/Swords/LightSword.cs
@@ -5,12 +5,15 @@ public class LightSword : Sword
 {
     public GameObject lightLaserPrefab;
     public ParticleSystem particleLight = null;
+    public Light light = null;
 
     public override void Start() {
         base.Start();
         base.swordType = SwordType.LIGHT;
         particleLight = GetComponentsInChildren<ParticleSystem>()[0];
+        light = GetComponentsInChildren<Light>()[0];
         particleLight.Stop();
+        light.enabled = false;
     }
 
     public override void Attack() {
@@ -22,6 +25,7 @@ public class LightSword : Sword
         base.Ability();
         // Light torch
         particleLight.Play();
+        light.enabled = true;
     }
 
     public override void OnTriggerEnter2D(Collider2D collision) {
