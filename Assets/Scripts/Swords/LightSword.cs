d273473092ae0e76e2224a05252d6058ad5d3fff
DPigeon <D_Pig@encs.concordia.ca>
2019-11-12 03:07:54 -0500

    Light Particles when Right clicking for ability

--- Assets/Scripts/Swords/LightSword.cs
+++ Assets/Scripts/Swords/LightSword.cs
@@ -4,18 +4,24 @@ using System.Collections;
 public class LightSword : Sword
 {
     public GameObject lightLaserPrefab;
+    public ParticleSystem particleLight = null;
 
     public override void Start() {
         base.Start();
         base.swordType = SwordType.LIGHT;
+        particleLight = GetComponentsInChildren<ParticleSystem>()[0];
+        particleLight.Stop();
     }
 
     public override void Attack() {
         base.Attack();
+        // Laser
     }
 
     public override void Ability() {
         base.Ability();
+        // Light torch
+        particleLight.Play();
     }
 
     public override void OnTriggerEnter2D(Collider2D collision) {
