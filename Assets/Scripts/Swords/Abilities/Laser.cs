5bc01135678b9daf487202483ee5a1d1796a440c
DPigeon <D_Pig@encs.concordia.ca>
2019-11-12 03:50:37 -0500

    #40: Inheritance of Projectiles with boss spell, lasers

---
+++ Assets/Scripts/Swords/Abilities/Laser.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : Projectile {
+
+    public override void Awake() {
+        base.Awake();
+    }
+
+    public override void SetDirection(Vector2 direction) {
+        base.SetDirection(direction);
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+
+    }
+
+}
