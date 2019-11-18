16d0a5b534c060bf60919797d7f0616f0eebe1e7
DPigeon <D_Pig@encs.concordia.ca>
2019-11-17 20:57:35 -0500

    #76: Lava Tile Pushback force

---
+++ Assets/Scripts/LavaTile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaTile : MonoBehaviour {
+	Rigidbody2D playerRigidbody;
+	bool touchingLava;
+	
+	float pushbackForce = 20.0F;
+	
+    public virtual void Start() {
+		playerRigidbody = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+		touchingLava = false;
+    }
+
+    public virtual void Update() { 
+		
+    }
+	
+	void OnTriggerEnter2D(Collider2D col) {
+        if (col.tag == "Player") {
+            touchingLava = true;
+			playerRigidbody.AddForce(new Vector2(0.0F, pushbackForce), ForceMode2D.Impulse);
+        }
+    }
+	
+	void OnTriggerExit2D(Collider2D col) {
+        if (col.tag == "Player") {
+            touchingLava = false;
+        }
+    }
+	
+}
