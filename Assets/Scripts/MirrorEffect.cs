cdee66404882c3f237b6360c25bb78ecc110a1c2
DPigeon <D_Pig@encs.concordia.ca>
2019-11-19 02:01:15 -0500

    #78 #69 Mirrors effects & second puzzle

---
+++ Assets/Scripts/MirrorEffect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorEffect : MonoBehaviour {
+	[SerializeField]
+	GameObject mobPrefab;
+	
+	AudioSource mirrorPuzzleSound; // For last target showing puzzle hint
+
+    void Start() {
+		AudioSource[] audioSources = GetComponents<AudioSource>();
+        mirrorPuzzleSound = audioSources[0];
+    }
+
+    void Update() {
+    }
+	
+	void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.name == "Laser(Clone)" && gameObject.tag == "Mirror") {
+            collider.gameObject.GetComponent<Laser>().SetDirection(transform.right);
+        }
+		
+		if (collider.name == "Laser(Clone)" && gameObject.tag == "BrokenMirror") {
+			mirrorPuzzleSound.Play();
+            Destroy(collider.gameObject);
+			
+			// For level 3 --> spawns a mob as hint
+			if (gameObject.transform.position == new Vector3(50.65F, -3.89F, 0.0F)) {
+				GameObject mob = Instantiate(mobPrefab, new Vector3(50.65F, -5.22F, 0.0F), Quaternion.identity) as GameObject;
+				mob.transform.localScale = new Vector3(4.0F, 4.0F, 4.0F);
+			}
+        }
+    }
+	
+}
