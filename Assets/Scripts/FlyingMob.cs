6e10f9ddf20389615842cdbec34697963bb96c53
DPigeon <D_Pig@encs.concordia.ca>
2019-11-23 15:49:27 -0500

    #73: Enemy Flying towards player with range

---
+++ Assets/Scripts/FlyingMob.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingMob : Enemy {
+	[SerializeField]
+	Player player;
+	[SerializeField]
+	public float moveSpeed;
+	[SerializeField]
+	public float playerRange;
+	
+	public LayerMask playerLayer;
+	public bool playerInRange;
+	public bool followOnAway;
+	
+    public override void Start() {
+        player = GameObject.Find("Player").GetComponent<Player>();
+    }
+
+    public override void Update() {
+		playerInRange = Physics2D.OverlapCircle(transform.position, playerRange, playerLayer);
+		if (playerInRange && !followOnAway) {
+			transform.position = Vector3.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
+			return;
+		}
+    }
+	
+	// Disable the gizmo in the game
+	void OnDrawGizmosSelected() {
+		// Makes a circle around player for playerRange
+		Gizmos.DrawSphere(transform.position, playerRange);
+	}
+	
+}
