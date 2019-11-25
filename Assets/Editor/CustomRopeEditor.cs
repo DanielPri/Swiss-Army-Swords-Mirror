244e356c6cc9b7abef8f528ad0fee781b865222d
DanielPri <privorotskyd@gmail.com>
2019-11-25 15:54:19 -0500

    [#31] added hinges, made cage a prefab

--- Assets/Editor/CustomRopeEditor.cs
+++ Assets/Editor/CustomRopeEditor.cs
@@ -8,9 +8,9 @@ public class CustomRopeEditor : Editor
 {
     public override void OnInspectorGUI()
     {
+        EditorGUILayout.HelpBox("HOW TO USE: Place this script on an empty object, then give it a rope segment prefab. Indicate amount of segments you want and click generate.", MessageType.Info);
         DrawDefaultInspector();
-        EditorGUILayout.HelpBox("Click button after setting parameters", MessageType.Info);
-
+        
         GenerateRope myScript = (GenerateRope)target;
         if (GUILayout.Button("Generate Rope"))
         {
