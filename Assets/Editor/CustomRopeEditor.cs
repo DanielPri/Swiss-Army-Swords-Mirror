d7651f13a0ce1a44ee16316d7a397dd02ef12f44
DanielPri <privorotskyd@gmail.com>
2019-11-25 15:26:39 -0500

    [#31] created rope generator Script

---
+++ Assets/Editor/CustomRopeEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GenerateRope))]
+public class CustomRopeEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        EditorGUILayout.HelpBox("Click button after setting parameters", MessageType.Info);
+
+        GenerateRope myScript = (GenerateRope)target;
+        if (GUILayout.Button("Generate Rope"))
+        {
+            myScript.createRopesQty();
+        }
+
+        if (GUILayout.Button("Reset"))
+        {
+            myScript.Reset();
+        }
+    }
+}
+
---
+++ Assets/Scripts/GenerateRope.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GenerateRope : MonoBehaviour
+{
+
+    [SerializeField] public
+    GameObject ropeSegmentPrefab;
+    
+    public int qty;
+    public float yOffset = 0.4375f;
+
+    private GameObject cachedSegment;
+
+    public void createRopesQty()
+    {
+        Reset();
+        float offset = 0f;
+        for (int i = 0; i < qty; i++)
+        {
+            GameObject clone = Instantiate(ropeSegmentPrefab, transform);
+            clone.transform.position = new Vector3(transform.position.x, transform.position.y + offset, transform.position.z);
+            offset += yOffset;
+
+            if(i > 0)
+            {
+                var hinge = clone.GetComponent<HingeJoint2D>();
+                hinge.connectedBody = cachedSegment.GetComponent<Rigidbody2D>();
+            }
+            cachedSegment = clone;
+        }
+    }
+
+    public void Reset()
+    {
+        List<Transform> childList = new List<Transform>();
+        foreach (Transform child in transform)
+        {
+            childList.Add(child);
+        }
+        foreach (Transform child in childList)
+        {
+            DestroyImmediate(child.gameObject, true);
+        }
+    }
+}
