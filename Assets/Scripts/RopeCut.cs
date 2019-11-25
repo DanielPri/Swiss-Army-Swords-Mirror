3c7b82288b82c652b090711297511e00fbc19c06
DanielPri <privorotskyd@gmail.com>
2019-11-25 16:34:08 -0500

    [#31] rope can be cut at any point

---
+++ Assets/Scripts/RopeCut.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeCut : MonoBehaviour
+{
+    [SerializeField]
+    HingeJoint2D ropeSegment;
+
+    void Start()
+    {
+        GameObject.Find("Player");
+
+    }
+    
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.tag == "Sword")
+        {
+            Sword swordScript = col.GetComponent<Sword>();
+            if (swordScript.damaging)
+            {
+                transform.parent.gameObject.GetComponent<HingeJoint2D>().enabled = false;
+            }
+        }
+        
+    }
+
+}
