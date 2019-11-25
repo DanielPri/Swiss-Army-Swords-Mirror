6756666c579de0eeabd4f11e0a7fd8a01e2699a1
DPigeon <D_Pig@encs.concordia.ca>
2019-11-24 20:32:39 -0500

    Sign Hint with Dialogue Panel

--- Assets/Scripts/GamePuzzles.cs
+++ Assets/Scripts/GamePuzzles.cs
@@ -10,6 +10,19 @@ using UnityEngine;
 public class GamePuzzles : MonoBehaviour {
 	[SerializeField]
 	public GameObject flameSwordDropPrefab = null; // Level 3 drop
-	
+	[SerializeField]
+    GameObject hintSignUI = null;
 	public int rightMirrorCounter = 0; // Level 3: For the 4th broken mirror puzzles
+	
+	void Start() {
+		hintSignUI.SetActive(false);
+	}
+	
+	void OnTriggerEnter2D(Collider2D col) {
+        hintSignUI.SetActive(true);
+    }
+	
+	void OnTriggerExit2D(Collider2D col) {
+        hintSignUI.SetActive(false);
+    }
 }
