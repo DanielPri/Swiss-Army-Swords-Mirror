80b65c898e8513b33524f2b886a09da7ed872a46
DanielPri <privorotskyd@gmail.com>
2019-11-25 23:12:32 -0500

    [#31] added snow element (visuals only)

--- Assets/Scripts/RopeCut.cs
+++ Assets/Scripts/RopeCut.cs
@@ -3,16 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RopeCut : MonoBehaviour
-{
-    [SerializeField]
-    HingeJoint2D ropeSegment;
-
-    void Start()
-    {
-        GameObject.Find("Player");
-
-    }
-    
+{   
     void OnTriggerStay2D(Collider2D col)
     {
         if (col.tag == "Sword")
