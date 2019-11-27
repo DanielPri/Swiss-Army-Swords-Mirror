f4af16addd762dfba00a3e5443d4fa08a711f13b
DPigeon <D_Pig@encs.concordia.ca>
2019-11-27 04:22:58 -0500

    #16: Scene Transition to Level 1 once done

---
+++ Assets/Scripts/CutsceneManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+
+public class CutsceneManager : MonoBehaviour {
+	public PlayableDirector director;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        if (director.state != PlayState.Playing) { // If cutscene done, move to level 1
+			SceneManager.LoadScene("Level 1");
+		}
+    }
+}
