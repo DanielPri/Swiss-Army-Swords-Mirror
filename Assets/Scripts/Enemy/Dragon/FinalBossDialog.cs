c9dde6705114f3c3643a7b75b57744344ad695bf
Kevin Luu <kevinateur@hotmail.com>
2019-11-30 02:06:49 -0500

    [#86] - Add dialogue

---
+++ Assets/Scripts/Enemy/Dragon/FinalBossDialog.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinalBossDialog : MonoBehaviour
+{
+    public Queue<string> sentences;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/Utility/DialogueManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager instance;
+    public Text nameText;
+    public Text dialogueText;
+    private Queue<string> sentences;
+
+    public static DialogueManager Instance { get { return instance; } }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue (Dialogue dialogue)
+    {
+        nameText.text = dialogue.name;
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+}
