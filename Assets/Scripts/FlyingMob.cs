31f912463cd4306df1db63367386ae372f6d4e49
Maude <maude.braunstein@gmail.com>
2019-11-30 02:52:15 -0500

    #68: More to the level. Currently a bug with the mob light shrinking

--- Assets/Scripts/FlyingMob.cs
+++ Assets/Scripts/FlyingMob.cs
@@ -26,7 +26,6 @@ public class FlyingMob : Enemy { // 3 HP, Gives 3 Damages
     float hurtTimer = 0.0F;
     float hurtDuration = 2.0F;
 	AudioSource hitSound;
-    Light light;
 
 
     new void Start() {
@@ -41,26 +40,26 @@ public class FlyingMob : Enemy { // 3 HP, Gives 3 Damages
         AudioSource[] audioSources = GetComponents<AudioSource>();
         hitSound = audioSources[0];
         lightsword = FindObjectOfType<LightSword>();
-        light = GetComponentInChildren<Light>();
     }
 
     public override void Update() {
 		// Must be attracted to light of light sword
 		base.Update();
-		playerInRange = Physics2D.OverlapCircle(transform.position, playerRange, playerLayer);
+        if (easyMobHP < 1) {
+            Die();
+            return;
+        }
+        playerInRange = Physics2D.OverlapCircle(transform.position, playerRange, playerLayer);
 		sword = GameObject.FindGameObjectWithTag("Sword").GetComponent<Sword>();
         // For level 2, they should be attracted to light of light sword
         if (lightsword && playerInRange && FindObjectOfType<LightSword>().laserOn == true && sceneName.Contains("Level 2")) { 
-			if (easyMobHP < 1)
-				Die();
+
 			transform.position = Vector3.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
 			FaceDirection(player.transform.position);
 			return;
 		}
         else if (!lightsword && sceneName == "Level 2")
         { // For first half of level 2, if there's no sword light, they should just move on patrol regardless of where the player is
-            if (easyMobHP < 1)
-                Die();
             if (movingRight)
                 transform.Translate(Vector2.right * speed * Time.deltaTime);
             else
@@ -68,15 +67,11 @@ public class FlyingMob : Enemy { // 3 HP, Gives 3 Damages
             return;
         }
         else if (playerInRange && sceneName == "Level 3") {
-			if (easyMobHP < 1)
-				Die();
 			transform.position = Vector3.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
 			FaceDirection(player.transform.position);
 			return;
 		}
 		HandleTimers();
-		if (easyMobHP < 1)
-            Die();
     }
 	
 	public override void Die() {
@@ -109,17 +104,25 @@ public class FlyingMob : Enemy { // 3 HP, Gives 3 Damages
     }
 	
 	private void FaceDirection(Vector3 playerPosition) {
+        // This will separate the light from the mob when it moves around to chase the player, to
+        // prevent the child object (its light) from flipping/changing as well.
+        // This was causing the light to zoom in too close to the object and creating really ugly light
+        List<Transform> children = new List<Transform>();
+        foreach (Transform child in transform)
+        {
+            child.parent = null;
+            children.Add(child);
+        }
         // Opposite to the player
-        if (transform.position.x < player.transform.position.x) {
-            // Using eulerAngles to flip sprite will prevent the child object (its light) from flipping/changing as well.
-            // This was causing the light to zoom in too close to the object and creating really ugly light
-            transform.eulerAngles = new Vector3(0, 0, 0); 
+        if (transform.position.x < player.transform.position.x) {         
+            Vector3 newScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, 1);
+            transform.localScale = newScale;
         }
         else {
-            transform.eulerAngles = new Vector3(0, 180, 0);
+            Vector3 newScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, 1);
+            transform.localScale = newScale;
         }
-        // Keep light object's position set
-        //light.gameObject.transform.localScale = new Vector3(transform.position.x, transform.position.y, -0.205f);
+        foreach (Transform child in children) child.parent = transform;
     }
 
     // Disable the gizmo in the game
