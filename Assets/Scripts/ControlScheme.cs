7c4369b748437a9218a83666c9ad1140dcb1d9eb
Patrick Pin <patrickpin840@hotmail.com>
2019-11-30 20:26:02 -0500

    Adding a control scheme

---
+++ Assets/Scripts/ControlScheme.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlScheme : MonoBehaviour
+{
+    Camera mainCamera;
+    Canvas ui;
+    CanvasRenderer panel;
+
+    void Start()
+    {
+        mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        ui = GameObject.Find("ControlScheme").GetComponent<Canvas>();
+        panel = GameObject.Find("ControlSchemePanel").GetComponent<CanvasRenderer>();
+        ui.worldCamera = mainCamera;
+    }
+
+    void Update()
+    {
+        panel.SetAlpha(0.7f);
+    }
+}
