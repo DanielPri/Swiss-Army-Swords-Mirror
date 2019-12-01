49490399f4d8865dfd3a06850466d53a480e1914
Patrick Pin <patrickpin840@hotmail.com>
2019-12-01 02:00:24 -0500

    [#64] Functional pause button, add pause handler, fix UI prefab

---
+++ Assets/Pause.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    GameObject ui;
+    GameObject pauseMenu;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject); // prevent from getting destroyed between scenes
+    }
+
+    void Start()
+    {
+        ui = GameObject.Find("UI Canvas");
+        ui.SetActive(true);
+        pauseMenu = GameObject.Find("ControlScheme");
+        pauseMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseMenu.activeInHierarchy)
+            {
+                ContinueGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    private void PauseGame()
+    {
+        Time.timeScale = 0;
+        ui.SetActive(false);
+        pauseMenu.SetActive(true);
+    }
+
+    private void ContinueGame()
+    {
+        Time.timeScale = 1;
+        ui.SetActive(true);
+        pauseMenu.SetActive(false);
+    }
+}
