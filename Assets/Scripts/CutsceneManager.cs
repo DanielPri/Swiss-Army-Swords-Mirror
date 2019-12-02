0319013054e4634c3869c6bf85f1e75101dcf0e5
DPigeon <D_Pig@encs.concordia.ca>
2019-12-02 09:03:54 -0500

    Cutscene Time Logic

--- Assets/Scripts/CutsceneManager.cs
+++ Assets/Scripts/CutsceneManager.cs
@@ -6,17 +6,28 @@ using UnityEngine.SceneManagement;
 
 public class CutsceneManager : MonoBehaviour {
 	public PlayableDirector director;
+	string sceneName;
 
     void Start() {
-        
+        Scene currentScene = SceneManager.GetActiveScene(); // To know which level
+		sceneName = currentScene.name;
     }
 
     void Update() {
-        if (director.time > 28.4) { // If cutscene done, move to level 1 (28 seconds)
-			SceneManager.LoadScene("Level 1");
-		}
-		if (Input.GetButton("SkipCutscene")) { // Skip cutscene by pressing "s"
-			director.time = 28.0;
+		if (sceneName == "Cutscene") {
+			if (director.time > 28.4) { // If cutscene done, move to level 1 (28 seconds)
+				SceneManager.LoadScene("Level 1");
+			}
+			if (Input.GetButton("SkipCutscene") ) { // Skip cutscene by pressing "s"
+				director.time = 28.0;
+			}
+		} else if (sceneName == "FinalCutscene") {
+			if (director.time > 40.0) { // If cutscene done, move to level 1 (28 seconds)
+				SceneManager.LoadScene("MainMenu");
+			}
+			if (Input.GetButton("SkipCutscene") ) { // Skip cutscene by pressing "s"
+				director.time = 40.0;
+			}
 		}
     }
 }
