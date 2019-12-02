9e69acba6191cc90f290739e7d2f8553368cf726
DanielPri <privorotskyd@gmail.com>
2019-12-01 23:11:11 -0500

    lever is good now

--- Assets/Scripts/LeverDoorController.cs
+++ Assets/Scripts/LeverDoorController.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class LeverDoorController : MonoBehaviour
 {
 
+    Animator anim;
+
     [SerializeField] List<GameObject> Doors;
     [SerializeField] float timeDelay = 0.5f;
     float timeSinceLastToggle = 0.5f;
+
    public void toggle() { 
         if(timeSinceLastToggle > timeDelay)
             foreach (GameObject Door in Doors) {
@@ -15,7 +18,13 @@ public class LeverDoorController : MonoBehaviour
                 timeSinceLastToggle = 0;
                 Door.GetComponent<Door>().toggle();
                 Debug.Log("YOU'RE IN MY LAYER, uhhh..... lever.");
+                anim.SetTrigger("toggle");
             }
+    }
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
     }
 
     void Update()
