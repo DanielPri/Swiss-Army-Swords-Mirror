19df1840940dbf1b057b9b849723db75fb1566a3
Kevin Luu <kevinateur@hotmail.com>
2019-12-02 09:27:58 -0500

    LavaBoss transition added

---
+++ Assets/Scripts/Utility/SceneChanger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class SceneChanger : MonoBehaviour
+{
+    [SerializeField]
+    private string nameOfTheScene;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            SceneManager.LoadScene(nameOfTheScene);
+        }
+    }
+
+}
