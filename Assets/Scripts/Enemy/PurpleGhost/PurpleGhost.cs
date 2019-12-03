80bef82f3a9c315f12e8bce43c020c7d4e044814
Kevin Luu <kevinateur@hotmail.com>
2019-12-02 23:21:33 -0500

    finalize

--- Assets/Scripts/Enemy/PurpleGhost/PurpleGhost.cs
+++ Assets/Scripts/Enemy/PurpleGhost/PurpleGhost.cs
@@ -6,10 +6,13 @@ public class PurpleGhost : BossParent
     [SerializeField]
     private GameObject lightningPrefab;
 
-    private bool attacking1, isDead, isInvulnerable;
+    private bool attacking1, attacking2, isDead, isInvulnerable, toWait;
     private Light light;
     float hurtTimer, invulnerabilityTimer;
     float hurtDuration = 0.6F;
+
+    Rigidbody2D rg;
+    HitpointBar playerBar;
 
     BossBar hitpointBar;
     SpriteRenderer sr;
@@ -19,30 +22,44 @@ public class PurpleGhost : BossParent
         player = GameObject.FindGameObjectWithTag("Player");
         sr = GetComponent<SpriteRenderer>();
         light = GameObject.Find("PurpleGhostLightEmission").GetComponent<Light>();
+        rg = GetComponent<Rigidbody2D>();
+        playerBar = GameObject.Find("HitpointBar").GetComponent<HitpointBar>();
     }
 
     private void Update()
     {
-        sword = GameObject.FindGameObjectWithTag("Sword").GetComponent<Sword>();
         hitpointBar = GameObject.Find("BossLifeBar(Clone)").GetComponent<BossBar>();
+        sword = GameObject.FindGameObjectWithTag("Sword").GetComponent<Sword>();
 
         FaceDirection(player.transform.position);
         HandleTimers();
 
         if (CheckIfInRange(7f) && !attacking1 && !isDead)
         {
-            StartCoroutine(LightOff());
             StartCoroutine(SpawnLightningShock());
         }
+
+        if (CheckIfInRange(17f) && !attacking2 && !isDead)
+        {
+            StartCoroutine(LightOff());
+        }
+
     }
 
     IEnumerator SpawnLightningShock()
     {
         attacking1 = true;
-        yield return new WaitForSeconds(7f);
         GameObject a = Instantiate(lightningPrefab) as GameObject;
         a.transform.position = player.transform.position;
+        yield return new WaitForSeconds(7f);
         attacking1 = false;
+    }
+
+    IEnumerator Wait()
+    {
+        toWait = true;
+        yield return new WaitForSeconds(7f);
+        toWait = false;
     }
 
     IEnumerator LightOff()
@@ -67,8 +84,9 @@ public class PurpleGhost : BossParent
     {
         if (col.gameObject.name == "Player")
         {
-            Vector2 forceDirection = new Vector2(facingDirection.x, 1.0f) * 2f;
+            Vector2 forceDirection = new Vector2(facingDirection.x, 1.0f);
             player.GetComponent<Rigidbody2D>().AddForce(forceDirection, ForceMode2D.Impulse);
+            playerBar.DecreaseHitpoint(1);
         }
     }
 
@@ -77,20 +95,29 @@ public class PurpleGhost : BossParent
         return (Vector2.Distance(player.transform.position, transform.position) <= range);
     }
 
+    private void LateUpdate()
+    {
+        if (hitpointBar.GetHP() < 1)
+            Die();
+    }
+
     void Die()
     {
         isDead = true;
         Destroy(this);
+        gameObject.SetActive(false);
     }
 
     private void HandleTimers()
     {
-        if (CheckIfInRange(3))
+        if (CheckIfInRange(3) && !toWait)
         {
             // Follow the player
             Vector2 target = player.transform.position - transform.position;
             transform.Translate(target.normalized * speed * Time.deltaTime, Space.World);
             FaceDirection(player.transform.position);
+            StartCoroutine(Wait());
+
         }
         else if(CheckIfInRange(5)) // move to height of player to accurately shoot projectiles
         {
@@ -128,8 +155,6 @@ public class PurpleGhost : BossParent
                 invulnerabilityTimer = 0.0f;
             }
         }
-        if (hitpointBar.GetHP() < 1)
-            Die();
     }
 
     private void OnTriggerStay2D(Collider2D col)
@@ -137,8 +162,10 @@ public class PurpleGhost : BossParent
         if (col.tag == "Sword" && sword.damaging && !isInvulnerable)
         {
             isHurt = true;
-            hitpointBar.DecreaseBossHitpoint(2);
+            hitpointBar.DecreaseBossHitpoint(5);
             isInvulnerable = true;
+            Vector2 forceDirection = new Vector2(facingDirection.x, 1.0f) * 2f;
+            rg.AddForce(forceDirection, ForceMode2D.Impulse);
         }
     }
 
